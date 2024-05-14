6186f8d470b89ad4ed643087c04291215d3280c8
ud9211 <111651727+ud9211@users.noreply.github.com>
2024-05-14 06:43:52 +0000

    java files

---
+++ java/palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int r, sum = 0, temp;
+        int n = 454;  // It is the number variable to be checked for palindrome
+
+        temp = n;
+        while (n > 0) {
+            r = n % 10;           // Getting remainder
+            sum = (sum * 10) + r;
+            n = n / 10;
+        }
+
+        if (temp == sum) {
+            System.out.println("Palindrome number");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
